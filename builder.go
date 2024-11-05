8c0f223aebb4fa4e020c11d975806ad48cda73b0
Mohammed Al Sahaf <msaa1990@gmail.com>
2024-11-05 22:19:43 +0000

    fix default build tags inclusion
    Signed-off-by: Mohammed Al Sahaf <msaa1990@gmail.com>

--- builder.go
+++ builder.go
@@ -152,7 +152,7 @@ func (b Builder) Build(ctx context.Context, outputFile string) error {
 			cmd.Args = append(cmd.Args,
 				"-ldflags", "-w -s", // trim debug symbols
 				"-trimpath",
-				"-tags", "nobadger", "nomysql", "nopgx",
+				"-tags", "nobadger,nomysql,nopgx",
 			)
 		}
 	}
